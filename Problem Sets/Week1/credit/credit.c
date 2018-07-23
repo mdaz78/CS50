aa6a3d4217f500844303545283dc744b3001241d
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-07-23 15:24:23 +0530

    Change variable name

--- Problem Sets/Week1/credit/credit.c
+++ Problem Sets/Week1/credit/credit.c
@@ -11,9 +11,8 @@ int main(void) {
     cc_number = get_long_long("Number: ");
   }
   while(cc_number < 0);
-
   int sum = sum_up_every_second_digit(cc_number);
-  printf("%i\n", sum);
+
 }
 
 
@@ -24,14 +23,14 @@ int sum_up_every_second_digit(long long int temp_cc_number) {
   {
     int temp_sum = 0;
     int pair = temp_cc_number % 100;
-    int second_digit_from_pair = pair / 10;
-    int second_digit_from_pair_times_two = second_digit_from_pair * 2;
-    if (second_digit_from_pair_times_two > 10) {
-      int first_no = second_digit_from_pair_times_two % 10;
-      int second_no = second_digit_from_pair_times_two / 10;
+    int first_digit_from_pair = pair / 10;
+    int first_digit_from_pair_times_two = first_digit_from_pair * 2;
+    if (first_digit_from_pair_times_two > 10) {
+      int first_no = first_digit_from_pair_times_two % 10;
+      int second_no = first_digit_from_pair_times_two / 10;
       temp_sum = first_no + second_no;
     } else {
-      temp_sum = second_digit_from_pair_times_two;
+      temp_sum = first_digit_from_pair_times_two;
     }
     temp_cc_number = temp_cc_number / 100;
     sum += temp_sum;

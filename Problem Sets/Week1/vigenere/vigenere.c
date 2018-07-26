019df5a58fa858bd6ed990c6b04ab6d3be9503bb
Md Abu Zafar <mdabuzafar78@gmail.com>
2018-07-26 10:42:13 +0530

    Ensure user entered two args and the second args is a pure string

--- Problem Sets/Week1/vigenere/vigenere.c
+++ Problem Sets/Week1/vigenere/vigenere.c
@@ -3,8 +3,30 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+
+string cipher(string plaintext, string key);
 
 int main(int argc, string argv[])
 {
+  // validate the input
+  if (argc == 2) {
+    string key = argv[1];
+    for (int i = 0, n = strlen(key); i < n; i++) {
+      if (!isalpha(key[i])) {
+        printf("The key can not be non alphabetical characters\n");
+        return 1;
+      }
+    }
+    // if everything goes well ask for input
+    string plaintext = get_string("plaintext: ");
+  } else {
+    printf("The input should be exactly two\n");
+    return 1;
+  }
+}
 
+// cipher the input using the key
+string cipher(string plaintext, string key) 
+{
 }
